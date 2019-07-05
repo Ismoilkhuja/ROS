79d532ab861b45935388dd6f608ee53bf908cee6
Ismoilkhuja <nuriddinov-ismoil@outlook.com>
2019-07-05 07:34:50 +0000

    Initial commit

---
+++ build/my_turtle/CMakeFiles/my_turtle_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_turtle_node.dir/src/my_turtle_node.cpp.o"
+  "/SummerSchool2019/catkin_ws/devel/lib/my_turtle/my_turtle_node.pdb"
+  "/SummerSchool2019/catkin_ws/devel/lib/my_turtle/my_turtle_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_turtle_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/my_turtle/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /SummerSchool2019/catkin_ws/src/my_turtle
+# Build directory: /SummerSchool2019/catkin_ws/build/my_turtle
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
